a33d288d9a97637cb56e60eeb8953a499780980b
Ian Halliday <ianhall@microsoft.com>
2016-12-06 01:54:16 -0800

    Naive hello world scanner implementation
    Get the ball rolling, yeah!
    
    Also for reference:
    http://codereview.stackexchange.com/questions/149075/what-is-the-rust-idiomatic-way-to-write-this-naive-javascript-token-scanner-code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,61 @@
+enum Token {
+    Identifier,
+    StringLiteral,
+    LeftParenthesis,
+    RightParenthesis,
+    Semicolon,
+}
+
+mod ast {
+    enum Node {
+        Identifier,
+        StringLiteral,
+        CallExpression,
+    }
+}
+
+fn scan_identifier(first: char, chars: &mut std::str::Chars) {
+    let mut identifier = String::new();
+    identifier.push(first);
+
+    let mut id_chars = chars.clone();
+    while let Some(c) = id_chars.next() {
+        match c {
+            'a'...'z'|'A'...'Z'|'0'...'9' =>
+                identifier.push(c),
+            _ => break,
+        }
+    }
+    println!("{}", identifier);
+    chars.nth(identifier.chars().count() - 2);
+}
+
+fn scan_string(chars: &mut std::str::Chars) {
+    let mut string = String::new();
+    while let Some(c) = chars.next() {
+        match c {
+            '\'' => { println!("'{}'", string); return; },
+            _ => string.push(c),
+        }
+    }
+    println!("unterminated string! '{}", string);
+}
+
+fn scan(src: &str) {
+    let mut chars = src.chars();
+    while let Some(c) = chars.next() {
+        match c {
+            '(' => println!("("),
+            ')' => println!(")"),
+            ';' => println!(";"),
+            '\'' => scan_string(&mut chars),
+            _ => scan_identifier(c, &mut chars),
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let input = "print('Hello, world!');";
+    println!("Input:\n\n{}\n\nTokens:\n", input);
+    scan(input);
 }
